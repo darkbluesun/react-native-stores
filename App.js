9fdf3c6adf53c664584d35630b1f6c3c622bdfec
Chris Young <chris@chris-young.com>
2019-02-07 12:43:21 +1100

    Display list of stores from hardcoded url

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+export default class StoresList extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true }
+  }
+
+  componentDidMount(){
+    return fetch('http://0.0.0.0:8000/api/stores')
+      .then((response) => response.json())
+      .then((dataSource) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource,
+        });
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+  render(){
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return(
+      <View style={{flex: 1, flexDirection: 'column', paddingTop: 20}}>
+        <Text style={{fontSize: 24}}>Stores:</Text>
+        <View style={{paddingTop:20}}>
+          <FlatList
+            data={this.state.dataSource}
+            renderItem={({item}) => <Store name={item.name} branches={item.branches} />}
+            keyExtractor={({id}, index) => id.toString()}
+          />
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class Store extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>- { this.props.name }</Text>
+        <FlatList style={{paddingLeft: 10}}
+          data={this.props.branches}
+          renderItem={({item}) => <Store name={item.name} branches={item.branches} />}
+          keyExtractor={({id}, index) => id.toString()}
+        />
+      </View>
+    )
+  }
+}
